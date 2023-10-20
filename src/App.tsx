2716d6997f80a16149ca08addbbab6d706fa9344
jasoncUD <jasonc@udel.edu>
2023-10-20 14:56:03 -0400

    Finished task 10

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,14 @@ import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { DoubleHalf } from "./bad-components/DoubleHalf";
-import { ChooseTeam } from "./bad-components/ChooseTeam";
 import { ColoredBox } from "./bad-components/ColoredBox";
 import { ShoveBox } from "./bad-components/ShoveBox";
 import { ChooseTeam } from "./bad-components/ChooseTeam";
+import { CheckAnswer } from "./form-components/CheckAnswer";
+import { GiveAttempts } from "./form-components/GiveAttempts";
+import { EditMode } from "./form-components/EditMode";
+import { ChangeColor } from "./form-components/ChangeColor";
+import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
 
 function App(): JSX.Element {
     return (
@@ -46,6 +50,19 @@ function App(): JSX.Element {
             <ColoredBox></ColoredBox>
             <hr />
             <ShoveBox></ShoveBox>
+            <hr />
+            <CheckAnswer expectedAnswer={"George Washington"}></CheckAnswer>
+            <hr />
+            <GiveAttempts></GiveAttempts>
+            <hr />
+            <EditMode></EditMode>
+            <hr />
+            <ChangeColor></ChangeColor>
+            <hr />
+            <MultipleChoiceQuestion
+                options={["Value 2", "2", "New York", "Maryland"]}
+                expectedAnswer="2"
+            ></MultipleChoiceQuestion>
         </div>
     );
 }
--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,49 @@
 import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [numAttempts, setNumAttempts] = useState<number>(3);
+    const [reqAttempts, setReqAttempts] = useState<string>("");
+    const [isButtonDisabled, setButtonDisabled] = useState(false);
+
+    function decreaseNumToZero() {
+        if (numAttempts > 1) {
+            setNumAttempts(numAttempts - 1);
+        } else if (numAttempts === 1) {
+            setNumAttempts(numAttempts - 1);
+            setButtonDisabled(true);
+        } else {
+            setButtonDisabled(true);
+        }
+    }
+
+    function increaseNumAttempts() {
+        if (!Number.isNaN(parseInt(reqAttempts))) {
+            setNumAttempts(numAttempts + parseInt(reqAttempts));
+            if (numAttempts + parseInt(reqAttempts) > 0) {
+                setButtonDisabled(false);
+            }
+        }
+    }
+
     return (
         <div>
-            <h3>Give Attempts</h3>
+            <Form.Group controlId="formReqAttempts">
+                <Form.Label>Input the amount of attempts you want:</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={reqAttempts}
+                    placeholder="Input a number here"
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setReqAttempts(event.target.value)
+                    }
+                />
+            </Form.Group>
+            <Button onClick={decreaseNumToZero} disabled={isButtonDisabled}>
+                use
+            </Button>
+            <Button onClick={increaseNumAttempts}>gain</Button>
+            <h3>Number of Attempts Remaining: {numAttempts}</h3>
         </div>
     );
 }
